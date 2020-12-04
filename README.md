04133dce595da93af3916dda0e30af23c40103ca
Brian Cain <brianccain@gmail.com>
2020-12-03 22:18:37 -0800

    Initial commit of Utilities

---
+++ README.md
@@ -0,0 +1,1 @@
+# Simple Timer Unity Coroutine
---
+++ Utilities.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public static class Utilities {
+  /*
+   * Simple way to start functions after a timer runs out
+   *
+   * StartCoroutine(DoAfter(5f, ()=>EndGame()));
+   */
+  private IEnumerator DoAfter(float delay, System.Action operation) {
+    yield return new WaitForSeconds(delay);
+    operation();
+  }
+}
